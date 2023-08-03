9cfdd7976b6711458427a0cec595a2787068cbc7
Ryan <ryan.godin@live.com>
2023-08-03 19:52:02 -0400

    basicpointscommand

---
+++ commands/player/points.js
@@ -0,0 +1,34 @@
+const { Client, Interaction, EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { color, footer} = require('../../configs/embeds');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('points')
+        .setDescription('View points per win, loss, kill, loss & mvp.'),
+
+    async execute(client, interaction){
+        
+        const pointsEmbed = new EmbedBuilder()
+            .setColor(color)
+            .setAuthor({name: 'Points', iconURL: client.user.displayAvatarURL()})
+            .setTimestamp()
+            .setFooter({text: footer, iconURL: client.user.displayAvatarURL()});
+
+        const cachePoints = client.ranks.points;
+
+        const points = {
+            win: ['win', cachePoints.win],
+            loss: ['loss', cachePoints.loss],
+            kill: ['kill', cachePoints.kill],
+            death: ['death', cachePoints.death],
+            mvp: ['mvp', cachePoints.mvp]
+        };
+
+        for (const point in points){
+            
+            pointsEmbed.addFields({name: point, value: String(points[point][1]), inline: true})
+        }
+
+        return await interaction.reply({embeds : [pointsEmbed]});
+    }
+}
